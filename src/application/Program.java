f3726285225c3e4b7ec9bf5f82495afecc6e29e4
Silas Augusto <silasaugusto300@gmail.com>
2021-08-18 17:06:32 -0300

    Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position position = new Position(1,2);
-		System.out.println(position);
+
+		Board board = new Board(8, 8); 
+
 	}
 
 }
