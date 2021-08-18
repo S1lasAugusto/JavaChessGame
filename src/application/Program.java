bdfacf4d1f08db67024ce995db25f545efbc29dc
Silas Augusto <silasaugusto300@gmail.com>
2021-08-18 16:44:59 -0300

    Adding position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		Position position = new Position(1,2);
+		System.out.println(position);
 	}
 
 }
